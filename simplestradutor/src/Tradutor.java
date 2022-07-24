dc068da9caeeac65c564715f980e045c38cd1d91
sergio costa <sergio.costa@ufma.com>
2022-07-24 14:41:02 -0300

    incluido identificado

--- simplestradutor/src/Tradutor.java
+++ simplestradutor/src/Tradutor.java
@@ -9,6 +9,14 @@ class Scanner {
         this.input = input;
     }
 
+    private void skipWhitespace() {
+        char ch = peek();
+        while (ch == ' ' || ch == '\r' || ch == '\t' || ch == '\n') {
+            advance();
+            ch = peek();
+        }
+    }
+
 
     private char peek () {
         if (current < input.length)
@@ -23,6 +31,16 @@ class Scanner {
         }
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+                c == '_';
+}
+    
+private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || Character.isDigit((c));
+}
+
     private Token number() {
         int start = current ;
         while (Character.isDigit(peek())) {
@@ -33,9 +51,25 @@ class Scanner {
         return new Token(TokenType.NUMBER, n);
     }
 
+    private Token identifier() {
+        int start = current;
+        while (isAlphaNumeric(peek())) advance();
+    
+        String id = new String(input, start, current-start)  ;
+        return new Token(TokenType.IDENT, id);
+    }
+
 
     public Token nextToken () {
+
+        skipWhitespace();
+
         char ch = peek();
+
+        if (isAlpha(ch)) {
+            return identifier();
+        }
+
         if (ch == '0') {
             advance();
             return new Token (TokenType.NUMBER, Character.toString(ch));
@@ -56,18 +90,18 @@ class Scanner {
                 case '\0':
                     return new Token (TokenType.EOF,"EOF");
                 default:
-                     throw new Error("lexical error");
+                    throw new Error("lexical error at " + ch);
         }
     }
 
     
 
 }
-/* 
+
  class Parser {
     
     private Scanner scan;
-    private String currentToken;
+    private Token currentToken;
 
     public Parser(byte[] input) {
         scan = new Scanner(input);
@@ -84,41 +118,39 @@ class Scanner {
     }
  
 
-    private void match(char t) {
-        if (currentToken == t) {
+    private void match(TokenType t) {
+        if (currentToken.type == t) {
             nextToken();
         }else {
             throw new Error("syntax error");
         }
-   }
+    }
+
+    void number () {
+        System.out.println("push " + currentToken.lexeme);
+        match(TokenType.NUMBER);
+    }
 
 
     void expr() {
-        digit();
+        number();
         oper();
-     }
-
-     void digit () {
-        if (Character.isDigit(currentToken)) {
-            System.out.println("push " + currentToken);
-            match(currentToken);
-        } else {
-           throw new Error("syntax error");
-        }
     }
 
+ 
+
     void oper () {
-        if (currentToken == '+') {
-            match('+');
-            digit();
+        if (currentToken.type == TokenType.PLUS) {
+            match(TokenType.PLUS);
+            number();
             System.out.println("add");
             oper();
-        } else if (currentToken == '-') {
-            match('-');
-            digit();
+        } else if (currentToken.type == TokenType.MINUS) {
+            match(TokenType.MINUS);
+            number();
             System.out.println("sub");
             oper();
-        } else if (currentToken == '\0') {
+        } else if (currentToken.type == TokenType.EOF) {
             // vazio
         } else {
             throw new Error("syntax error");
@@ -126,24 +158,17 @@ class Scanner {
     }
 
 }
- */
+ 
 
 
 
 public class Tradutor {
     public static void main(String[] args) {
-        //String input = "8+5-7+9";
-       // Parser p = new Parser (input.getBytes());
-        //p.parse();
+        
+        String input = "45  + preco - 876";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
 
-        String input = "289-85+0+69";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
+   
     }
 }
